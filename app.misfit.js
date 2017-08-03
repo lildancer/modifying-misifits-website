3fd120a87954efc071893dfc13df85895b2b03c4
lildancer <littlecarmel3@gmail.com>
2017-08-03 14:13:39 -0600

    8.3.17
    Here is a more advanced version of what is occuring on my website. More modifications to come

--- app.misfit.js
+++ app.misfit.js
@@ -1,3 +1,21 @@
-$("document").ready(function(){
-  $(".dropdown-button").dropdown();
+$("document").ready(function() {
+      $ ("paths").hover(function($ ("arrive").show(function())));
+
+  function initMap() {
+    var waterfall = {lat: 51.929089, lng:-8.570961};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 17,
+      center: waterfall,
+      heading: 90,
+      tilt: 45,
+       mapTypeId: 'satellite'
+
+      // 51.9315975,-8.5683233
+    });
+    var marker = new google.maps.Marker({
+      position: waterfall,
+      map: map
+    });
+  }
+  $("#paths").hover(function ($ ("#arrive").show(function)));
 })
